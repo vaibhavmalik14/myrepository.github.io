00bc820f202c8e7f6f3dca7c9065738724e84554
vaibhavmalik14 <90602277+vaibhavmalik14@users.noreply.github.com>
2022-05-23 15:25:14 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -73,7 +73,7 @@ export default function NavBar() {
                   zIndex: -1,
                 }}
               >
-                <NextLink href="/">🤝BetterFund</NextLink>
+                <NextLink href="/">FundME</NextLink>
               </Box>
             </Heading>
           </Flex>
